b212e2852a7f61b942a2d435611834e722b69798
eunnzy <silverg12@skuniv.ac.kr>
2023-01-15 23:22:10 +0900

    project settings

---
+++ Here-Is-My-Home/src/main/java/com/guardian/myhome/controller/IndexController.java
@@ -0,0 +1,27 @@
+package com.guardian.myhome.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@RequestMapping("/")
+public class IndexController {
+	
+	@RequestMapping("index")
+	public String home(Locale locale, Model model) {
+		
+		return "index";
+	}
+	
+}
---
+++ Here-Is-My-Home/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>메인 화면</title>
+</head>
+<body>
+	<h1> Hello world!  </h1>
+	
+	<p> main 페이지가 될 파일 </p>
+</body>
+</html>
